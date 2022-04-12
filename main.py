6302d8f73a1df8a69e6dfbf0b756f5d9d88aafd6
1taroh <65810214+1taroh@users.noreply.github.com>
2022-04-12 12:52:23 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+number = 19
+chapter = 4
+for i in range(1,number+1):
+    print(str(chapter)+"."+str(i))
+print("5章")
+print("問い")
+number = 26
+chapter = 5
+for i in range(1,number+1):
+    print(str(chapter)+"."+str(i))
+print("6章")
+print("問い")
+number = 10
+chapter = 6
+for i in range(1,number+1):
+    print(str(chapter)+"."+str(i))
+print("練習問題")
+number = 3
+chapter = 6
+for i in range(1,number+1):
+    print(str(chapter)+"."+str(i))
